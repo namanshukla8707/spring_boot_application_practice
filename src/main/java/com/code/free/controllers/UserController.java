a01711ce5883d852476efd9ac847ae00d4ae6a6a
JaiswalHarsh321 <connect.harsh17@gmail.com>
2025-11-23 16:37:40 +0000

    Open Ai and Validate user name api in progress

--- src/main/java/com/code/free/controllers/UserController.java
+++ src/main/java/com/code/free/controllers/UserController.java
@@ -1,16 +1,30 @@
 package com.code.free.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.code.free.responses.ValidUsernameResponseDto;
+import com.code.free.services.UserService.UserService;
+import com.code.free.utilities.ApiResult;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
     
-    @GetMapping("/info")
-    public String getUserInfo(){
-        return "User Info";
+    private final UserService userService;
+
+    @GetMapping("/validate-name")
+    public ApiResult<ValidUsernameResponseDto> validateUsername(@RequestParam("username") String username){
+
+       return userService.validateUsername(username);
     }
+   
+
 
 }
--- src/main/java/com/code/free/services/UserService/UserService.java
+++ src/main/java/com/code/free/services/UserService/UserService.java
@@ -1,23 +1,54 @@
 package com.code.free.services.UserService;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.code.free.entities.user.UserView;
 import com.code.free.repositories.user.UserRepo;
+import com.code.free.repositories.user.UserViewRepo;
+import com.code.free.responses.CustomResponse;
+import com.code.free.responses.ValidUsernameResponseDto;
+import com.code.free.utilities.ApiResult;
+import com.code.free.utilities.OpenAIUtils;
+
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService{
+public class UserService implements UserDetailsService {
 
     private final UserRepo userRepo;
+    private final UserViewRepo userViewRepo;
+    private final OpenAIUtils openAIUtils;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         return userRepo.findByUsername(username).orElseThrow();
+    }
+
+    public ApiResult<ValidUsernameResponseDto> validateUsername(String username) {
+        UserView user = userViewRepo.findByUsername(username).orElse(null);
+        ValidUsernameResponseDto response = new ValidUsernameResponseDto();
+        String message = "";
+        if (user != null) {
+            response.setIsExist(true);
+            List<String> suggestions = List.of(username + "123", username + "_user", "the_" + username);
+            response.setSuggestions(suggestions);
+            openAIUtils.aiUse();
+            
+            message = "Username is already taken. Here are some suggestions.";
+        }
+        else {
+            response.setIsExist(false);
+            message = "Username is available.";
+        }
+        return CustomResponse.success(response,message,HttpStatus.OK); 
     }
 }
