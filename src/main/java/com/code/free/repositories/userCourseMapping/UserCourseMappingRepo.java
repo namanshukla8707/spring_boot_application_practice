7ab0f1b18907192c63542764471a41504008de7b
Naman Shukla <namanshukla8707@gmail.com>
2025-12-08 19:41:07 +0530

    course upload and view flow done

--- src/main/java/com/code/free/repositories/userCourseMapping/UserCourseMappingRepo.java
+++ src/main/java/com/code/free/repositories/userCourseMapping/UserCourseMappingRepo.java
@@ -3,10 +3,15 @@ package com.code.free.repositories.userCourseMapping;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.code.free.entities.userCourseMapping.UserCourseMappingEntity;
 import com.code.free.entities.userCourseMapping.UserCourseMappingId;
 
 public interface UserCourseMappingRepo extends JpaRepository<UserCourseMappingEntity, UserCourseMappingId> {
     Optional<UserCourseMappingEntity> findByUserIdAndCourseId(Long userId, Long courseId);
+
+    @Query(value = "SELECT COUNT(*) > 0 FROM user_course_mapping WHERE user_id = :userId AND course_id = :courseId AND is_active = true", nativeQuery = true)
+    Boolean existsMapping(@Param("userId") Long userId, @Param("courseId") Long courseId);
 }
---
+++ src/main/java/com/code/free/responses/VideoResponses/VideoResponseInterfaceDto.java
@@ -0,0 +1,8 @@
+package com.code.free.responses.VideoResponses;
+
+public interface VideoResponseInterfaceDto {
+    String getTitle();
+    String getDescription();
+    Integer getPosition();
+    String getUrl();
+}
