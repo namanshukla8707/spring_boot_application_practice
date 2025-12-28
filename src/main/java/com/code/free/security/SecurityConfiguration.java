68a3e792b884eb68de341a7954b00a7e49f3939b
Naman Shukla <namanshukla8707@gmail.com>
2025-12-29 02:41:19 +0530

    cors added

--- src/main/java/com/code/free/security/SecurityConfiguration.java
+++ src/main/java/com/code/free/security/SecurityConfiguration.java
@@ -6,30 +6,53 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
 import static com.code.free.utilities.globalEnums.RoleType.*;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final JwtAuthFilter jwtAuthFilter;
-    private final FilterChainAccessDeniedExpections filterChainAccessDeniedExpections;
+        private final JwtAuthFilter jwtAuthFilter;
+        private final FilterChainAccessDeniedExpections filterChainAccessDeniedExpections;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrfConfig -> csrfConfig.disable())
-                .sessionManagement(
-                        sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-                        .requestMatchers("/api/v1/courses/**").hasAnyRole(TEACHER.name(),ADMIN.name())
-                        .anyRequest().authenticated())
-                        .exceptionHandling(expection->expection.accessDeniedHandler(filterChainAccessDeniedExpections))
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.cors(cors -> {
+                }).csrf(csrfConfig -> csrfConfig.disable())
+                                .sessionManagement(
+                                                sessionConfig -> sessionConfig
+                                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
+                                                .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+                                                .requestMatchers("/api/v1/courses/**")
+                                                .hasAnyRole(TEACHER.name(), ADMIN.name())
+                                                .anyRequest().authenticated())
+                                .exceptionHandling(expection -> expection
+                                                .accessDeniedHandler(filterChainAccessDeniedExpections))
+                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                return http.build();
+        }
 
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("*"));
+                configuration.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+
+                return source;
+        }
 }
